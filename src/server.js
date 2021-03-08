ed000a456ffad4eddbd2f9a8f374c9eeb488677b
kalita-trevi <kalitatrevi@icloud.com>
2021-03-07 22:27:21 -0300

    Testando aplicacao express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("hello", Math.random())
+const express = require('express')
+const app = express()
+const port = 3000 // identificador da sua aplicacao
+
+app.get('/', (req, res) => { // instancia uma rota na instancia app;
+  res.send('Hello World!') // retorna mensagem ao solicitante (res)
+})
+
+app.listen(port, () => { // inicia o servidor
+  console.log(`Example app listening at http://localhost:${port}`) // mensagem de sucesso
+})
