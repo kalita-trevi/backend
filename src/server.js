a9894ca92372ab6b0c6787c2dc9d2c35a4a91584
kalita-trevi <kalitatrevi@icloud.com>
2021-03-10 23:27:56 -0300

    nodemon, frases  e front teste

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000 // identificador da sua aplicacao
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const port = 3000; // identificador da sua aplicacao
 
-app.get('/', (req, res) => { // instancia uma rota na instancia app;
-  res.send('Hello World!') // retorna mensagem ao solicitante (res)
-})
+// server.get('/:teste', (req, res) => { // instancia uma rota na instancia server;
+//   res.send(req.params.teste) // retorna mensagem ao solicitante (res)
+// })
+// https://expressjs.com/en/resources/middleware/cors.html
+// https://imasters.com.br/back-end/desenvolvendo-logica-da-sua-aplicacao-com-controllers-laravel
 
-app.listen(port, () => { // inicia o servidor
-  console.log(`Example app listening at http://localhost:${port}`) // mensagem de sucesso
-})
+const frases = [{
+    curtidas: 44,
+    frase: 'Escolhe um trabalho de que gostes e não terás que trabalhar nem um dia na tua vida.',
+    autor: 'confusio'
+}, {
+    curtidas: 853845,
+    frase: 'O homem superior atribui a culpa a si próprio; o homem comum, aos outros.',
+    autor: 'confusio'
+}, {
+    curtidas: 3534566,
+    frase: 'Não ser descoberto numa mentira é o mesmo que dizer a verdade.',
+    autor: 'Aristoteles Onassis'
+}];
+
+server.use(cors());
+
+server.get('/frases', (require, response) => {
+    response.json(frases)
+});
+
+server.listen(port, () => { // inicia o servidor
+    console.log(`Iniciar servidor da Kiu 🥰 em: http://localhost:${port}`); // mensagem de sucesso
+});
